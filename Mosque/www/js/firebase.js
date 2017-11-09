e454299b2e1d55e95291cf4e3ce95a01a3b683ee
irontamoor <tamoorahmed@ymail.com>
2017-11-09 12:57:20 +0000

    v

---
+++ Mosque/www/js/firebase.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+  // TODO: Replace with your project's customized code snippet
+  var config = {
+    apiKey: "AIzaSyCQi7owEJXClpxx92KSseKpAYN1FrLrMTU",
+    authDomain: "mosque-76018.firebaseapp.com",
+    databaseURL: "https://mosque-76018.firebaseio.com/",
+    storageBucket: "mosque-76018.appspot.com",
+    messagingSenderId: "855562353122",
+      };
+
+  firebase.initializeApp(config);
